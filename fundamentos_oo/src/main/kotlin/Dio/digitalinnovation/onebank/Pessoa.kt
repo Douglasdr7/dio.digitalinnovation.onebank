c268368dda62d4885cbf220960ee554173ff6e20
douglas <douglas.dr.ramos@gmail.com>
2022-06-19 14:43:20 -0300

    Updated to apply the private modifier to set funcion of cpf property

--- fundamentos_oo/src/main/kotlin/Dio/digitalinnovation/onebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/Dio/digitalinnovation/onebank/Pessoa.kt
@@ -4,9 +4,11 @@ class Pessoa {
 
     var nome: String = "Douglas"
     var cpf:String = "347.023.648-80"
+    private set
 }
 fun main(){
     val Douglas = Pessoa()
     println(Douglas.nome)
     println(Douglas.cpf)
-}
+
+   }
