25f877a9e0d440aaf8506232587df007fb3e1a43
douglas <douglas.dr.ramos@gmail.com>
2022-06-19 09:27:48 -0300

    add Pessoa

---
+++ fundamentos_oo/src/main/kotlin/Dio/digitalinnovation/onebank/Pessoa.kt
@@ -0,0 +1,12 @@
+package Dio.digitalinnovation.onebank
+
+class Pessoa {
+
+    var nome: String = "Douglas"
+    var cpf:String = "347.023.648-80"
+}
+fun main(){
+    val Douglas = Pessoa()
+    println(Douglas.nome)
+    println(Douglas.cpf)
+}
