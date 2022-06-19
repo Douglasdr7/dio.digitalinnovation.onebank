c528af9e82caf345a2e472f8d895af7d0eef1553
douglas <douglas.dr.ramos@gmail.com>
2022-06-19 15:09:12 -0300

    Updated to talk about class members

--- fundamentos_oo/src/main/kotlin/Dio/digitalinnovation/onebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/Dio/digitalinnovation/onebank/Pessoa.kt
@@ -5,10 +5,12 @@ class Pessoa {
     var nome: String = "Douglas"
     var cpf:String = "347.023.648-80"
     private set
+
+    constructor ()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 fun main(){
     val Douglas = Pessoa()
-    println(Douglas.nome)
-    println(Douglas.cpf)
-
+    println(Douglas.pessoaInfo())
    }
